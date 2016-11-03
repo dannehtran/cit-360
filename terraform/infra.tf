3bb73b4f01472a63d455200e377c268ce89553b7
dannehtran <danny.tran.392@my.csun.edu>
2016-11-03 15:51:42 -0700

    Fully Built Infrastructure

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -18,10 +18,10 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-#Creates a public subnet with CIDR of "172.31.0.0/24" in us-west-2a
+#Creates a public subnet with CIDR of "172.31.12.0/24" in us-west-2a
 resource "aws_subnet" "public_subnet_a" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.0.0/24"
+  cidr_block = "172.31.12.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
   depends_on = ["aws_internet_gateway.gw"]
@@ -31,10 +31,10 @@ resource "aws_subnet" "public_subnet_a" {
     }
 }
 
-#Creates a public subnet with CIDR of "172.31.1.0/24" in us-west-2b
+#Creates a public subnet with CIDR of "172.31.13.0/24" in us-west-2b
 resource "aws_subnet" "public_subnet_b" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.1.0/24"
+  cidr_block = "172.31.13.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
   depends_on = ["aws_internet_gateway.gw"]
@@ -44,10 +44,10 @@ resource "aws_subnet" "public_subnet_b" {
   }
 }
 
-#Creates a public subnet with CIDR of "172.31.2.0/24" in us-west-2c
+#Creates a public subnet with CIDR of "172.31.14.0/24" in us-west-2c
 resource "aws_subnet" "public_subnet_c" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.2.0/24"
+  cidr_block = "172.31.14.0/24"
   availability_zone = "us-west-2c"
   map_public_ip_on_launch = true
   depends_on = ["aws_internet_gateway.gw"]
@@ -55,55 +55,6 @@ resource "aws_subnet" "public_subnet_c" {
   tags {
     Name = "public_c"
   }
-}
-
-#Creates a private subnet with CIDR of "172.31.3.0/22" in us-west-2a
-resource "aws_subnet" "private_subnet_a" {
-  vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.3.0/22"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = false
-
-  tags {
-    Name = "private_a"
-  }
-}
-
-#Creates a private subnet with CIDR of "172.31.7.0/22" in us-west-2b
-resource "aws_subnet" "private_subnet_b" {
-  vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.7.0/22"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = false
-
-  tags {
-    Name = "private_b"
-  }
-}
-
-#Creates a private subnet with CIDR of "172.31.11.0/22" in us-west-2c
-resource "aws_subnet" "private_subnet_c" {
-  vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.11.0/22"
-  availability_zone = "us-west-2c"
-  map_public_ip_on_launch = false
-
-  tags {
-    Name = "private_c"
-  }
-}
-
-#Creates an Elastic IP to be attached to the NAT Gateway
-resource "aws_eip" "nat" { 
-  instance = "${aws_instance.bastion.id}"
-  vpc = true
-}
-
-#Creates a NAT gateway with subnet_id "172.31.3.0/22"
-resource "aws_nat_gateway" "gw" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id = "${aws_subnet.private_subnet_a.id}"
-  depends_on = ["aws_internet_gateway.gw"]
 }
 
 #Creates a public route table in aws with the Internet Gateway
@@ -118,20 +69,6 @@ resource "aws_route_table" "public_routing_table" {
     Name = "public_routing_table"
   }
 }
-
-#Creates a private routing table in aws with the NAT Gateway
-resource "aws_route_table" "private_routing_table" {
-  vpc_id = "${var.vpc_id}"
-  route {
-  cidr_block = "0.0.0.0/0"
-  nat_gateway_id = "${aws_nat_gateway.gw.id}"
-  }
-
-  tags {
-    Name = "private_routing_table"
-  }
-}
-
 
 #Assosciates public_subnet_a to aws_route_table.public_routing_table
 resource "aws_route_table_association" "public_subnet_a_rt_assoc" {
@@ -149,6 +86,66 @@ resource "aws_route_table_association" "public_subnet_b_rt_assoc" {
 resource "aws_route_table_association" "public_subnet_c_rt_assoc" {
   subnet_id = "${aws_subnet.public_subnet_c.id}"
   route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+#Creates a private subnet with CIDR of "172.31.0.0/22" in us-west-2a
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.0.0/22"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "private_a"
+  }
+}
+
+#Creates a private subnet with CIDR of "172.31.4.0/22" in us-west-2b
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.4.0/22"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = false
+  
+  tags {
+    Name = "private_b"
+  }
+}
+
+#Creates a private subnet with CIDR of "172.31.8.0/22" in us-west-2c
+resource "aws_subnet" "private_subnet_c" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.8.0/22"
+  availability_zone = "us-west-2c"
+  map_public_ip_on_launch = false
+
+  tags {
+    Name = "private_c"
+  }
+}
+
+#Creates an Elastic IP to be attached to the NAT Gateway
+resource "aws_eip" "nat" { 
+  vpc = true
+}
+
+#Creates a NAT gateway with subnet_id of "aws_subnet.public_subnet_a"
+resource "aws_nat_gateway" "gw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.public_subnet_a.id}"
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+#Creates a private routing table in aws with the NAT Gateway
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = "${var.vpc_id}"
+  route {
+  cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "private_routing_table"
+  }
 }
 
 #Assosciates private_subnet_a to aws_route_table.private_routing_table
@@ -186,6 +183,12 @@ resource "aws_security_group" "ssh" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+#Creates an Elastic IP to be attached to the NAT Gateway
+resource "aws_eip" "bastion" {
+  instance = "${aws_instance.bastion.id}"
+  vpc = true
 }
 
 #Creates a single bastion EC2 instance in public_subnet_a
